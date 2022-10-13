583f2a63b797ee6393cab60de9a46537f99c12c0
Yury <yuriy-t87@yandex.com>
2022-10-13 21:46:17 +0300

    init

---
+++ main.py
@@ -0,0 +1,15 @@
+import controller as c
+import logging as log
+
+# если хотим, чтобы логи выводились в консоль
+log.basicConfig(filename='log.txt',
+                    filemode='a',
+                    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                    datefmt='%H:%M:%S',
+                    level=log.INFO)
+
+
+c.run(log)
+
+
+
